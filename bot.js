bb5c17d8437485a71be42e5e9c9456a1c1c2bff4
4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD <45238840+4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD@users.noreply.github.com>
2018-11-22 18:56:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
+   console.log(`.`);
+      console.log(`.`);
+        console.log(`.`);
+      console.log(`.`);
+    console.log(`.`);
+  console.log(`Starting...`);
 client.user.setGame(`anonymous`,"http://twitch.tv/anonymous")
-client.user.setStatus("dnd")
 });
 
 
