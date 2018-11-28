82c5dae74df109385db5b2772e4a421637fa824f
4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD <45238840+4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD@users.noreply.github.com>
2018-11-28 22:42:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "+";
+
 client.on('ready', () => {
-client.user.setGame(`Anonymous`,"http://twitch.tv/anonymous")
 });
-client.login("NTE0ODQxOTgwNTg5NzAzMTg3.DtgFwQ.M9jqHJOF3lqSs0jCzZHqjTOkd9c");
+
+client.on('ready', async() => {
+var server = "514822456998101002"; //
+var channel = "514822457706807312";//
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein**')
+    },305);
+ });
+
+ 
+client.login("NTE0ODQxOTgwNTg5NzAzMTg3.DuCAEw.J9aP7eDeEV3HzwT-mxEh5SzdK68");
